c528d9ec7504629c4a463dcce8c845c1462f89dd
Jon Q <hello@jonquach.com>
2021-02-09 21:35:19 -0500

    Add cover card CTA

---
+++ _patterns/hero-cover-card-cta.md
@@ -0,0 +1,33 @@
+---
+title: 'Cover Card with CTA'
+categories: ['Hero', 'Section']
+keywords: ['cover', 'picture', 'full', 'card', 'cta']
+date: '2021-02-09 18:48:00'
+---
+
+<!-- wp:cover {"url":"https://images.unsplash.com/photo-1585424529208-7bc775e92a74?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=1280&amp;q=80","id":52,"overlayColor":"black","minHeight":100,"minHeightUnit":"vh","align":"full"} -->
+<div class="wp-block-cover alignfull has-black-background-color has-background-dim" style="min-height:100vh"><img class="wp-block-cover__image-background wp-image-52" alt="" src="https://images.unsplash.com/photo-1585424529208-7bc775e92a74?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=1280&amp;q=80" data-object-fit="cover"/>
+
+<div class="wp-block-cover__inner-container"><!-- wp:cover {"overlayColor":"white"} -->
+<div class="wp-block-cover has-white-background-color has-background-dim">
+<div class="wp-block-cover__inner-container">
+
+<!-- wp:heading {"textAlign":"center","fontSize":"extra-large","textColor":"black"} -->
+<h2 class="has-text-align-center has-black-color has-text-color has-extra-large-font-size mt0"><strong>A better way to build</strong></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center","fontSize":"small","textColor":"black"} -->
+<p class="has-text-align-center has-black-color has-text-color has-small-font-size o-70 my0">Lorem ipsum dolor sit amet consect adipisicing elit. Possimus magnam voluptatum cupiditate veritatis in accusamus quisquam.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons {"contentJustification":"center"} -->
+<div class="wp-block-buttons is-content-justification-center"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link">Learn More</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons -->
+
+</div></div>
+<!-- /wp:cover -->
+
+</div></div>
+<!-- /wp:cover -->
--- pages/__wip.js
+++ pages/__wip.js
@@ -222,29 +222,32 @@ export default function Post({ post }) {
 }
 
 const patternContent = `
+<!-- wp:cover {"url":"https://images.unsplash.com/photo-1585424529208-7bc775e92a74?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=1280&amp;q=80","id":52,"overlayColor":"black","minHeight":100,"minHeightUnit":"vh","align":"full"} -->
+<div class="wp-block-cover alignfull has-black-background-color has-background-dim" style="min-height:100vh"><img class="wp-block-cover__image-background wp-image-52" alt="" src="https://images.unsplash.com/photo-1585424529208-7bc775e92a74?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=1280&amp;q=80" data-object-fit="cover"/>
 
-<!-- wp:cover {"url":"https://images.unsplash.com/photo-1585424529208-7bc775e92a74?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=1280&amp;q=80","dimRatio":40,"overlayColor":"black","minHeight":100,"minHeightUnit":"vh","contentPosition":"center center","align":"full"} -->
-<div class="wp-block-cover alignfull has-background-dim-40 has-black-background-color has-background-dim" style="min-height:100vh"><img class="wp-block-cover__image-background" alt="" src="https://images.unsplash.com/photo-1585424529208-7bc775e92a74?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=1280&amp;q=80" data-object-fit="cover"/>
+<div class="wp-block-cover__inner-container"><!-- wp:cover {"overlayColor":"white"} -->
+<div class="wp-block-cover has-white-background-color has-background-dim">
 <div class="wp-block-cover__inner-container">
 
-<!-- wp:group -->
-<div class="wp-block-group">
-<div class="wp-block-group__inner-container">
-
-<!-- wp:heading {"align":"center","textColor":"white"} -->
-<h2 class="f2 f1-l mt0 lh-solid has-text-align-center has-white-color has-text-color"><strong>A better way to build with blocks and patterns</strong></h2>
+<!-- wp:heading {"textAlign":"center","fontSize":"extra-large"} -->
+<h2 class="has-text-align-center has-extra-large-font-size mt0"><strong>A better way to build</strong></h2>
 <!-- /wp:heading -->
 
-<!-- wp:paragraph {"align":"center",,"textColor":"white","fontSize":"large","style":{"typography":{"lineHeight":"1.6"}}} -->
-<p class="has-large-font-size o-70 my0 has-text-align-center has-white-color has-text-color" style="line-height:1.6">Lorem ipsum dolor sit amet consect adipisicing elit. Possimus magnam voluptatum cupiditate veritatis in accusamus quisquam.</p>
+<!-- wp:paragraph {"align":"center","fontSize":"small"} -->
+<p class="has-text-align-center has-small-font-size o-70 my0">Lorem ipsum dolor sit amet consect adipisicing elit. Possimus magnam voluptatum cupiditate veritatis in accusamus quisquam.</p>
 <!-- /wp:paragraph -->
 
-</div></div>
-<!-- /wp:group -->
+<!-- wp:buttons {"contentJustification":"center"} -->
+<div class="wp-block-buttons is-content-justification-center"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link">Learn More</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons -->
 
 </div></div>
 <!-- /wp:cover -->
 
+</div></div>
+<!-- /wp:cover -->
 `;
 
 const post = {
